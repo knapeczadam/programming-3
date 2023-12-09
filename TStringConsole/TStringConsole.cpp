a252fee6283db6c7b5a6cc249e457bed105748db
Ádám Knapecz <knapecz.adam@gmail.com>
2023-12-09 19:34:12 +0100

    tstring console

--- TStringConsole/TStringConsole.cpp
+++ TStringConsole/TStringConsole.cpp
@@ -1,20 +1,27 @@
-// TStringConsole.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream> // cout,         wcout,        cin,  wcin
+#include <iomanip>  // setprecision, fixed
+#include <string>   // string,       wstring
+#include <sstream>  // stringstream, wstringstream
+#include <tchar.h>  // _T(),         _tmain,       TCHAR
 
-#include <iostream>
+#if defined(_UNICODE) or defined(UNICODE)
+    #define tstring std::wstring
+    #define tcout std::wcout
+    #define tcin std::wcin
+    #define tstringstream std::wstringstream
+#else
+    #define tstring std::string
+    #define tcout std::cout
+    #define tcin std::cin
+    #define tstringstream std::stringstream
+#endif
 
-int main()
+int _tmain(int argc, TCHAR* argv[])
 {
-    std::cout << "Hello World!\n";
+    double PI = 3.1415926535897932384626433832795;
+    tstringstream print;
+    tstring name = _T("John");
+    print << _T("Hello ") << name.c_str() << _T("!\n");
+    print << _T("PI = ") << std::setprecision(4) << std::fixed << PI << _T("\n");
+    tcout << print.str();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
