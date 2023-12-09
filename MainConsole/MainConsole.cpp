c35c4cb53c71b9095ab7ef4b95c0709237755cad
Ádám Knapecz <knapecz.adam@gmail.com>
2023-12-09 19:11:10 +0100

    Generic main

--- MainConsole/MainConsole.cpp
+++ MainConsole/MainConsole.cpp
@@ -1,20 +1,18 @@
-// MainConsole.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream> // std::cout, std::wcout
+#include <tchar.h> // _T(), TCHAR, _tmain()
 
-#include <iostream>
+#if defined(UNICODE) or defined(_UNICODE)
+    #define tcout std::wcout
+#else
+    #define tcout std::cout
+#endif
 
-int main()
+int _tmain(int argc, TCHAR* argv[])
 {
-    std::cout << "Hello World!\n";
+#if defined(UNICODE) or defined(_UNICODE)
+    std::wcout << L"int wmain(int argc, wchar_t* argv[])" << '\n';
+#else
+    std::cout << "int main(int argc, char* argv[])" << '\n';
+#endif
+    tcout << _T("size of TCHAR: ") << sizeof(TCHAR);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
