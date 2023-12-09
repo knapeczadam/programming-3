be0938cab5106710d5dd4c714e4e1295f3d2efa2
Ádám Knapecz <knapecz.adam@gmail.com>
2023-12-09 23:37:21 +0100

    Custom length function for tchar string

--- LengthConsole/LengthConsole.cpp
+++ LengthConsole/LengthConsole.cpp
@@ -9,11 +9,19 @@
 #define tcin std::cin
 #endif
 
+size_t length(const TCHAR* str)
+{
+    size_t len = 0;
+    while (str[len] != '\0') ++len;
+    return len;
+}
+
 int _tmain(int argc, TCHAR* argv[])
 {
     TCHAR cstr[] = _T("Hello, World!");
     tcout << _T("Length of string \"") << cstr << _T("\" is ") << _tcslen(cstr) << _T('\n');
     tcout << _T("Size of string \"") << cstr << _T("\" is ") << sizeof(cstr) << _T('\n');
+    tcout << _T("Length of string \"") << cstr << _T("\" is ") << length(cstr) << _T('\n');
     
     tcin.get();
     return 0;
