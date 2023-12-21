78cc4468381560b489e37180f68ed779ab85ddad
Ádám Knapecz <knapecz.adam@gmail.com>
2023-12-21 21:45:23 +0100

    Exam prep question 1 - variations

--- ExamPrepQuestion1Console/ExamPrepQuestion1Console.cpp
+++ ExamPrepQuestion1Console/ExamPrepQuestion1Console.cpp
@@ -2,19 +2,155 @@
 //
 
 #include <iostream>
+#include <tchar.h>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <ctime>
+#include <random>
+#include <cassert>
 
-int main()
+template <typename T>
+void DeleteMinMax(std::vector<T>& v)
 {
-    std::cout << "Hello World!\n";
+    assert(v.size() >= 2, "Vector must have at least 2 elements");
+    std::sort(std::begin(v), std::end(v));
+    v.erase(std::begin(v));
+    v.erase(std::end(v) - 1);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+template <typename callable, typename container>
+auto MeasureTimeV1(callable& func, container& data)
+{
+    constexpr int numMeasurements = 10;
+    std::vector<long long> measurements;
+    measurements.reserve(numMeasurements);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (int i = 0; i < numMeasurements; i++)
+    {
+        const auto start = std::chrono::high_resolution_clock::now();
+        func(data);
+        const auto end = std::chrono::high_resolution_clock::now();
+
+        const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        measurements.push_back(duration);
+    }
+
+    // Remove the largest and smallest measurements
+    DeleteMinMax(measurements);
+
+    // Calculate the average
+    const auto averageTime = std::accumulate(std::begin(measurements), std::end(measurements), 0.0) / static_cast<double>(measurements.size());
+    return averageTime;
+}
+
+
+template <typename callable, typename container>
+auto MeasureTimeV2(callable& func, container& data)
+{
+    constexpr int numMeasurements = 10;
+    std::vector<long long> measurements;
+    measurements.reserve(numMeasurements);
+
+    for (int i = 0; i < numMeasurements; i++)
+    {
+        const auto start = std::chrono::high_resolution_clock::now();
+        func(data);
+        const auto end = std::chrono::high_resolution_clock::now();
+
+        const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        measurements.push_back(duration);
+    }
+
+    // Remove the largest and smallest measurements
+    DeleteMinMax(measurements);
+
+    // Calculate the average
+    const auto averageTime = std::accumulate(std::begin(measurements), std::end(measurements), 0.0) / static_cast<double>(measurements.size());
+    return averageTime;
+}
+
+template <typename callable, typename container, typename time>
+auto MeasureTimeV3(callable& func, container& data)
+{
+    constexpr int numMeasurements = 10;
+    std::vector<long long> measurements;
+    measurements.reserve(numMeasurements);
+
+    for (int i = 0; i < numMeasurements; i++)
+    {
+        const auto start = std::chrono::high_resolution_clock::now();
+        func(data);
+        const auto end = std::chrono::high_resolution_clock::now();
+
+        const auto duration = std::chrono::duration_cast<time>(end - start).count();
+        measurements.push_back(duration);
+    }
+
+    // error-prone
+    // Remove the largest and smallest measurements
+    // const auto mixMax = std::minmax_element(std::begin(measurements), std::end(measurements));
+    // measurements.erase(mixMax.first);
+    // measurements.erase(mixMax.second);
+    DeleteMinMax(measurements);
+
+    // Calculate the average
+    const auto averageTime = std::accumulate(measurements.begin(), measurements.end(), 0.0) / static_cast<double>(measurements.size());
+    return averageTime;
+}
+
+struct MyCustomSort
+{
+    void operator()(std::vector<int>& v) const
+    {
+        std::sort(v.begin(), v.end(), [](int a, int b) { return a > b; });
+    }
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    // seed the random number generator
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    constexpr int numOfElements = 1000000;
+    std::vector<int> test(numOfElements);
+    for (int i = 0; i < numOfElements; i++)
+    {
+        test[i] = std::rand();
+    }
+
+    // Create the callable objects
+    auto myCustomSort1 = [](std::vector<int>& v)
+    {
+        std::sort(v.begin(), v.end(), [](int a, int b) { return a > b; });
+    };
+
+    // Create the callable objects
+    MyCustomSort myCustomSort2;
+
+    //--------------------------------------------------------------------------------
+    // Version 1
+    //--------------------------------------------------------------------------------
+    const auto averageTime1 = MeasureTimeV1(myCustomSort1, test);
+    std::cout << "Version 1\n";
+    std::cout << "Average time: " << averageTime1 << " milliseconds\n";
+    std::cout << "\n";
+
+    //--------------------------------------------------------------------------------
+    // Version 2
+    //--------------------------------------------------------------------------------
+    const auto averageTime2 = MeasureTimeV2(myCustomSort1, test);
+    std::cout << "Version 2\n";
+    std::cout << "Average time: " << averageTime2 << " milliseconds\n";
+    std::cout << "\n";
+    
+    //--------------------------------------------------------------------------------
+    // Version 3
+    //--------------------------------------------------------------------------------
+    const auto averageTime3 = MeasureTimeV3<MyCustomSort, std::vector<int>, std::chrono::microseconds>(myCustomSort2, test);
+    std::cout << "Version 3\n";
+    std::cout << "Average time: " << averageTime3 << " microseconds\n";
+    // std::cout << "Average time: " << averageTime3 / 1000.0 << " milliseconds\n";
+    std::cout << "\n";
+}
