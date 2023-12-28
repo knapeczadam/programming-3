b6219e5bbff749f34cae1e21f34984be9d0badb6
Ádám Knapecz <knapecz.adam@gmail.com>
2023-12-28 23:38:12 +0100

    Prep exam question 2 implementation

--- ExamPrepQuestion232/ExamPrepQuestion232.cpp
+++ ExamPrepQuestion232/ExamPrepQuestion232.cpp
@@ -4,12 +4,33 @@
 #include "framework.h"
 #include "ExamPrepQuestion232.h"
 
+#define _USE_MATH_DEFINES
+
+#include <vector>
+#include <cmath>
+#include <string>
+#include <sstream>
+
+
+#if defined(UNICODE) or defined(_UNICODE)
+#define tstring std::wstring
+#define tstringstream std::wstringstream
+#define tstr std::to_wstring
+#else
+#define tstring std::string
+#define tstringstream std::stringstream
+#define tstr std::to_string
+#endif
+
 #define MAX_LOADSTRING 100
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+TCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+std::vector<POINT> points;
+constexpr float gPI = 3.14159265358979323846f;
+int giNumOfPentagrams = 0;
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -17,9 +38,9 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
+                     _In_ LPTSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
@@ -28,8 +49,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // TODO: Place code here.
 
     // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_EXAMPREPQUESTION232, szWindowClass, MAX_LOADSTRING);
+    LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadString(hInstance, IDC_EXAMPREPQUESTION232, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
     // Perform application initialization:
@@ -64,7 +85,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+    WNDCLASSEX wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
@@ -76,11 +97,11 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_EXAMPREPQUESTION232));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_EXAMPREPQUESTION232);
+    wcex.lpszMenuName   = MAKEINTRESOURCE(IDC_EXAMPREPQUESTION232);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+    return RegisterClassEx(&wcex);
 }
 
 //
@@ -97,7 +118,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
@@ -125,6 +146,35 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+    case WM_ACTIVATE:
+        {
+            if (LOWORD(wParam) == WA_INACTIVE)
+            {
+                SetWindowText(hWnd, _T("This window is inactive; go away."));
+            }
+            else
+            {
+                SetWindowText(hWnd, _T("Heptragrams"));
+            }
+        }
+        break;
+    case WM_LBUTTONDOWN:
+        {
+            POINT pt;
+            pt.x = LOWORD(lParam);
+            pt.y = HIWORD(lParam);
+            points.push_back(pt);
+            ++giNumOfPentagrams;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+    case WM_RBUTTONDOWN:
+        {
+            points.clear();
+            giNumOfPentagrams = 0;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -146,7 +196,84 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
+
+            // Red, black pens
+            constexpr int lineWidth = 2;
+            HPEN hRedPen = CreatePen(PS_SOLID, lineWidth, RGB(255, 0, 0));
+            HPEN hBlackPen = CreatePen(PS_SOLID, lineWidth, RGB(0, 0, 0));
+
+            // Red, black brushes
+            HBRUSH hRedBrush = CreateSolidBrush(RGB(255, 0, 0));
+            HBRUSH hBlackBrush = CreateSolidBrush(RGB(0, 0, 0));
+
+            // Select red brush
+            // HBRUSH hOldBrush = static_cast<HBRUSH>(SelectObject(hdc, hRedBrush));
+            HBRUSH hOldBrush = static_cast<HBRUSH>(SelectObject(hdc, GetStockObject(NULL_BRUSH)));
+            // select red pen
+            HPEN hOldPen = static_cast<HPEN>(SelectObject(hdc, hBlackPen));
+
+            // set the brush to be transparent
+            SetBkMode(hdc, TRANSPARENT);
+
+            constexpr int iNumOfVertices = 9;
+            constexpr LONG radius = 50;
+            constexpr float angle = 2 * gPI / iNumOfVertices;
+
+            static_assert(iNumOfVertices % 2 == 1, "iNumOfVertices must be odd");
+
+            const float offsetAngle = gPI / 2 - angle - gPI;
+
+            if constexpr (iNumOfVertices < 3)
+            {
+                break;
+            }
+            for (int idx = 0; idx < points.size(); ++idx)
+            {
+                POINT pt = points[idx];
+                const POINT center = pt;
+                Ellipse(hdc, pt.x - radius, pt.y - radius, pt.x + radius, pt.y + radius);
+                for (int i = 0; i < iNumOfVertices; ++i)
+                {
+                    const float angle1 = angle * static_cast<float>(i) + offsetAngle;
+                    const float angle2 = angle * static_cast<float>(i + 2) + offsetAngle;
+                    const float fCenterX = static_cast<float>(center.x);
+                    const float fCenterY = static_cast<float>(center.y);
+                    const float fromX = fCenterX + radius * std::cos(angle1);
+                    const float fromY = fCenterY - radius * std::sin(angle1);
+                    const float toX = fCenterX + radius * std::cos(angle2);
+                    const float toY = fCenterY - radius * std::sin(angle2);
+
+                    MoveToEx(hdc, static_cast<int>(fromX), static_cast<int>(fromY), NULL);
+                    LineTo(hdc, static_cast<int>(toX), static_cast<int>(toY));
+                    
+                }
+                
+                // Draw the number of pentagrams
+                tstringstream ss;
+                // ss << pt.x << _T(", ") << pt.y;
+                ss << idx + 1;
+                tstring str = ss.str();
+                TextOut(hdc, pt.x - radius, pt.y - radius, str.c_str(), str.length());
+
+            }
+
+            tstringstream title;
+            title << _T("Heptragrams: ") << giNumOfPentagrams;
+            SetWindowText(hWnd, title.str().c_str());
+            // SendMessage(hWnd, WM_SETTEXT, 0, (LPARAM)title.str().c_str());
+
+            // Select the old brush into the device context
+            SelectObject(hdc, hOldBrush);
+
+            // Select the old pen into the device context
+            SelectObject(hdc, hOldPen);
+
+            // Cleanup
+            DeleteObject(hRedBrush);
+            DeleteObject(hBlackBrush);
+            DeleteObject(hRedPen);
+            DeleteObject(hBlackPen);
+            
             EndPaint(hWnd, &ps);
         }
         break;
